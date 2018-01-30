ac34dbd378b420030ebb538406264541320b8c15
Renzel Manlapaz <11906448+renzwthbenefits@users.noreply.github.com>
2018-01-30 09:28:55 +0800

    backwardify

--- browser.js
+++ browser.js
@@ -1,24 +1,27 @@
 'use strict';
-const isIp = require('is-ip');
 
-const defaults = {
+var isIp = require('is-ip');
+
+var defaults = {
 	timeout: 5000
 };
 
-const urls = {
+var urls = {
 	v4: 'https://ipv4.icanhazip.com/',
 	v6: 'https://ipv6.icanhazip.com/'
 };
 
 function queryHttps(version, opts) {
-	return new Promise((resolve, reject) => {
-		const doReject = () => reject(new Error('Couldn\'t find your IP'));
-		const xhr = new XMLHttpRequest();
+	return new Promise(function (resolve, reject) {
+		var doReject = function doReject() {
+			return reject(new Error('Couldn\'t find your IP'));
+		};
+		var xhr = new XMLHttpRequest();
 
 		xhr.onerror = doReject;
 		xhr.ontimeout = doReject;
-		xhr.onload = () => {
-			const ip = xhr.responseText.trim();
+		xhr.onload = function () {
+			var ip = xhr.responseText.trim();
 
 			if (!ip || !isIp[version](ip)) {
 				doReject();
@@ -33,12 +36,12 @@ function queryHttps(version, opts) {
 	});
 }
 
-module.exports.v4 = opts => {
+module.exports.v4 = function (opts) {
 	opts = Object.assign({}, defaults, opts);
 	return queryHttps('v4', opts);
 };
 
-module.exports.v6 = opts => {
+module.exports.v6 = function (opts) {
 	opts = Object.assign({}, defaults, opts);
 	return queryHttps('v6', opts);
 };
